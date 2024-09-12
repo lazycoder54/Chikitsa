cebdf021317e22ca84f3d054a05036be7026ed71
Pranjit Das <pranjitd865@gmail.com>
2024-09-12 20:33:17 +0530

    disable sentry for the time again

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-// import * as Sentry from "@sentry/nextjs";
+
 import { getUser } from "@/lib/actions/patient.actions";
 
 const Success = async ({
@@ -21,7 +21,7 @@ const Success = async ({
     (doc) => doc.name === appointment.primaryPhysician
   );
 
-  // Sentry.metrics.set("user_view_appointment-success", user.name);
+  
 
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
